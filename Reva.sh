808f220c1b0f0d0e2043c16630512d9078242879
sahandsame <58979786+sahandsame@users.noreply.github.com>
2020-06-09 17:38:45 +0300

    Add files via upload

---
+++ Reva.sh
@@ -0,0 +1,164 @@
+clear 
+
+cd .. 
+rm -rif usr/etc/motd
+cd $HOME
+
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      this tools Creat by REVA        # \e[0m\n"
+printf "\e[1;92m#                                      # \e[0m\n"
+printf "\e[1;92m#         student of shell             # \e[0m\n"
+printf "\e[1;92m#                                      # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 4
+pkg update -y
+clear
+pkg upgrade -y
+
+echo " REVA "
+sleep 1
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#            \ \    R    / /           # \e[0m\n"
+printf "\e[1;92m#             \ \   R    / /           # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#            / /    R    \ \           # \e[0m\n"
+printf "\e[1;92m#             / /   R    \ \           # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#           \ \    RE    / /           # \e[0m\n"
+printf "\e[1;92m#            \ \   RE    / /           # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#           / /    RE    \ \           # \e[0m\n"
+printf "\e[1;92m#            / /   RE    \ \           # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#           \ \    REV    / /          # \e[0m\n"
+printf "\e[1;92m#            \ \   REV    / /          # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#            / /   REV    \ \          # \e[0m\n"
+printf "\e[1;92m#            / /   REV    \ \          # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#          \ \    REVA    / /          # \e[0m\n"
+printf "\e[1;92m#           \ \   REVA    / /          # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#          / /    REVA    \ \          # \e[0m\n"
+printf "\e[1;92m#           / /   REVA    \ \          # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+
+
+
+echo"
+"
+termux-setup-storage
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#       BWASTA TA NANO DAXEL DABE      # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg intsall nano -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#       BWASTA TA PYTHON DAXEL DABE    # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install python -y
+sleep 1
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA PYTHON 2-3 DAXEL DABE # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install python2 -y
+pkg install python3 -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA GIT DAXEL DABE        # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install git -y
+sleep 1
+clear
+
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA FIGLET DAXEL DABE     # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install figlet -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA UNZIP DAXEL DABE      # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install unzip -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA ZIP DAXEL DABE        # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install zip -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA TOR DAXEL DABE       # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install tor -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA LIST UPGRADE DABE     # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+apt list --upgradable
+sleep 1 
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#      BWASTA TA discord.py daxel dabe # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pip install discord.py
+sleep 1
+clear
+cd $HOME
+cd ..
+rm -rif usr/etc/motd
+cd $HOME
+cd ..
+cd usr/etc
+mkdir REVA
+cd REVA
+echo SLAW BRAM AW TERMUXA CHAK KRAWA LA LAYAN REVA SWPAS BO BAKAR HENANE TOOLSAKA
+cd $HOME
+cd /sdcard
+mkdir REVA_FILE
+cd REVA_FILE
+echo SLAW BRAM AW TERMUXA CHAK KRAWA LA LAYAN REVA SWPAS BO BAKAR HENANE TOOLSAKA
+cd $HOME
+pkg install sl -y
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;4m#          follow my instagram          # \e[0m\n"
+printf "\e[1;94m#              @sh4hm                  # \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 15
+clear
+figlet good bye 
+sleep 2 
+clear
+ls
